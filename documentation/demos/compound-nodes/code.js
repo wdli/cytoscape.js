774af8562f2be3f0c84c983713ed97c15f5ef6f1
W. David Li <w.david.li@gmail.com>
2018-06-16 15:20:52 -0700

    update: playing with adding boxes in the example around nodes

--- documentation/demos/compound-nodes/code.js
+++ documentation/demos/compound-nodes/code.js
@@ -1,65 +1,67 @@
 var cy = window.cy = cytoscape({
-  container: document.getElementById('cy'),
+    container: document.getElementById('cy'),
 
-  boxSelectionEnabled: false,
-  autounselectify: true,
+    boxSelectionEnabled: false,
+    autounselectify: true,
 
-  style: [
-    {
-      selector: 'node',
-      css: {
-        'content': 'data(id)',
-        'text-valign': 'center',
-        'text-halign': 'center'
-      }
-    },
-    {
-      selector: '$node > node',
-      css: {
-        'padding-top': '10px',
-        'padding-left': '10px',
-        'padding-bottom': '10px',
-        'padding-right': '10px',
-        'text-valign': 'top',
-        'text-halign': 'center',
-        'background-color': '#bbb'
-      }
-    },
-    {
-      selector: 'edge',
-      css: {
-        'target-arrow-shape': 'triangle'
-      }
-    },
-    {
-      selector: ':selected',
-      css: {
-        'background-color': 'black',
-        'line-color': 'black',
-        'target-arrow-color': 'black',
-        'source-arrow-color': 'black'
-      }
-    }
-  ],
-
-  elements: {
-    nodes: [
-      { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
-      { data: { id: 'b' } },
-      { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
-      { data: { id: 'd' }, position: { x: 215, y: 175 } },
-      { data: { id: 'e' } },
-      { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } }
+    style: [
+	{
+	    selector: 'node',
+	    css: {
+		'content': 'data(id)',
+		'text-valign': 'center',
+		'text-halign': 'center'
+	    }
+	},
+	{
+	    selector: '$node > node',
+	    css: {
+		'padding-top': '10px',
+		'padding-left': '10px',
+		'padding-bottom': '10px',
+		'padding-right': '10px',
+		'text-valign': 'top',
+		'text-halign': 'center',
+		'background-color': '#bbb'
+	    }
+	},
+	{
+	    selector: 'edge',
+	    css: {
+		'target-arrow-shape': 'triangle'
+	    }
+	},
+	{
+	    selector: ':selected',
+	    css: {
+		'background-color': 'black',
+		'line-color': 'black',
+		'target-arrow-color': 'black',
+		'source-arrow-color': 'black'
+	    }
+	}
     ],
-    edges: [
-      { data: { id: 'ad', source: 'a', target: 'd' } },
-      { data: { id: 'eb', source: 'e', target: 'b' } }
 
-    ]
-  },
+    elements: {
+	nodes: [
+	    { data: { id: 'a', parent: 'b' }, position: { x: 215, y: 85 } },
+	    { data: { id: 'b' } },
+	    { data: { id: 'c', parent: 'b' }, position: { x: 300, y: 85 } },
+	    { data: { id: 'd', parent: 'g' }, position: { x: 215, y: 175 } },
+	    { data: { id: 'e' } },
+	    { data: { id: 'f', parent: 'e' }, position: { x: 300, y: 175 } },
+	    { data: { id: 'g' } },
+	],
+	edges: [
+	    { data: { id: 'ad', source: 'a', target: 'd' } },
+	    //{ data: { id: 'eb', source: 'e', target: 'b' } },
+	    { data: { id: 'af', source: 'a', target: 'f' } },
 
-  layout: {
-    name: 'preset',
-    padding: 5
-  }
+	]
+    },
+
+    layout: {
+	name: 'preset',
+	padding: 5
+    }
 });
